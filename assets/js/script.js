5b3d2a87babbd6311fe913b0f04f2dd8605ec63a
Rebecca Allford <rebeccaallford1@gmail.com>
2024-05-15 10:51:22 +0000

    Added function names to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+function rungame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractionQuestion(){
+
+}
+function MultiplyQuestion(){
+    
+}
