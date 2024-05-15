f27041213208ad852378bcb2017c48eb0a299cd8
Rebecca Allford <rebeccaallford1@gmail.com>
2024-05-15 12:14:08 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("you clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`you clicked ${gameType}`);
+                
+                runGame (gameType);
             }
         });
     }
+    runGame("addition");
 });
 
 
@@ -20,10 +22,17 @@ document.addEventListener("DOMContentLoaded", function () {
  * and after the users answer has been processed
   */
 
-function runGame() {
-    
+function runGame(gameType) {
+// Creates two random numbers between 1 and 25
 let num1 = Math.floor(Math.random() * 25) + 1;
 let num2 = Math.floor(Math.random() * 25) + 1;
+
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+}else{
+    alert(`unknown game type: ${gameType}`);
+    throw `unknown game type: ${gameType}. Aborting!`;
+}
 }
 function checkAnswer() {
 
@@ -37,8 +46,10 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+ document.getElementById("operand1").textContent = operand1;
+ document.getElementById("operand2").textContent = operand2;
+ document.getElementById("operator").textContent = "+";
 }
 function displaySubtractionQuestion() {
 
